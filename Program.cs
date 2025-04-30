a939b3be6545daa6a560bb421bf85d64b37e1e54
Luis De La Torre <dev.delatorre.l@gmail.com>
2025-04-29 19:07:22 -0500

    chnages of practice with requests and responses

--- Program.cs
+++ Program.cs
@@ -10,9 +10,15 @@ public class Program
         //app.MapGet("/", () => "Hello World!");
         app.Run(async (HttpContext context) =>
         {
-            context.Response.StatusCode = 400;
+            /*context.Response.StatusCode = 400;
             await context.Response.WriteAsync("hello");
-            await context.Response.WriteAsync(" world");
+            await context.Response.WriteAsync(" world");*/
+            string path = context.Request.Path;
+            string method = context.Request.Method;
+            context.Response.Headers["MyKey"] = "myValue";
+            context.Response.Headers.ContentType = "text/html";
+            await context.Response.WriteAsync($"<p>{path}</p>");
+            await context.Response.WriteAsync($"<p>{method}</p>");
         });
 
         app.Run();
